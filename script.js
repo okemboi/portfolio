0c3a04adf64b35697c58a95edcc9246d8b61af2b
CELYNEBIRUNGI <113887926+CELYNEBIRUNGI@users.noreply.github.com>
2023-04-18 15:10:15 +0300

    Fix javascript error

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const handleClick = () => {
   const mobileNavMenu = document.querySelector('#mobilemenu');
   const mobileMenuOverlay = document.querySelector('.mobile-menu-overlay');
-  mobileNavMenu.classList.toggle('mobile-menu-inactive');
-  mobileNavMenu.classList.toggle('mobile-menu');
+  hamburger.classList.toggle('mobile-menu-inactive');
+  hamburger.classList.toggle('mobile-menu');
   document.body.classList.toggle('menu-active');
   mobileMenuOverlay.classList.toggle('mobile-menu-overlay-active');
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#hamburger').addEventListener('click', handleClick);
+  document.querySelectorAll('.mobile-menu-links, .disabled-button').forEach((button) => button.addEventListener('click', handleClick));
+});
