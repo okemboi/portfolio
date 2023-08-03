af4252c51d3c3cac6a02ab783f6ce63f4119bff9
kkemboi <79087047+okemboi@users.noreply.github.com>
2023-08-03 18:13:05 +0300

    Add eslint local packages and fix eslint errors

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
-const container = document.getElementById("navMenu");
-const menuBtn = document.querySelector(".menuBtn")
-const headerIcon = document.querySelector(".header-icon")
+const container = document.getElementById('navMenu');
+const menuBtn = document.querySelector('.menuBtn');
+const headerIcon = document.querySelector('.header-icon');
 
 const showMenu = () => {
-  console.log("Clicked")
-  container.style.display = "block"
-}
+  container.style.display = 'block';
+};
 const hideMenu = () => {
-  container.style.display = "none"
-  console.log("Clicked")
-}
-menuBtn.addEventListener('click', hideMenu)
-headerIcon.addEventListener('click', showMenu)
+  container.style.display = 'none';
+};
+menuBtn.addEventListener('click', hideMenu);
+headerIcon.addEventListener('click', showMenu);
