0c3440544af795af2d468b0639b2e780cb17fe6b
Omar Gutierrez <omar.gt@icloud.com>
2023-08-03 08:16:49 -0600

    [Add] [JS functionality for menu to appear]

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ const container = document.getElementById("navMenu");
 const menuBtn = document.querySelector(".menuBtn")
 const headerIcon = document.querySelector(".header-icon")
 
+const showMenu = () => {
+  console.log("Clicked")
+  container.style.display = "block"
+}
+const hideMenu = () => {
+  container.style.display = "none"
+  console.log("Clicked")
+}
+menuBtn.addEventListener('click', hideMenu)
+headerIcon.addEventListener('click', showMenu)
