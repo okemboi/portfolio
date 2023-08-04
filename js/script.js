8c6988ccac8cc4aa1c7bd798cd12bf2ba3ec1c35
kkemboi <79087047+okemboi@users.noreply.github.com>
2023-08-04 14:26:14 +0300

    I corrected linters errors

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const container = document.getElementById('navMenu');
 const menuBtn = document.querySelector('.menuBtn');
 const headerIcon = document.querySelector('.header-icon');
-const portifolio =document.getElementById("portifoliobtn") 
-const about =document.getElementById("aboutbtn") 
-const contact =document.getElementById("contactbtn") 
+const portifolio = document.getElementById('portifoliobtn');
+const about = document.getElementById('aboutbtn');
+const contact = document.getElementById('contactbtn');
 
 const showMenu = () => {
   container.style.display = 'block';
@@ -14,6 +14,6 @@ const hideMenu = () => {
 menuBtn.addEventListener('click', hideMenu);
 headerIcon.addEventListener('click', showMenu);
 
-portifolio.addEventListener("click", hideMenu)
-about.addEventListener("click", hideMenu)
-contact.addEventListener("click", hideMenu)
+portifolio.addEventListener('click', hideMenu);
+about.addEventListener('click', hideMenu);
+contact.addEventListener('click', hideMenu);
